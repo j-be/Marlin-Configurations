7b186531bb27307bb2291518c09fbfd6ed8d7387
Juri Berlanda <juriberlanda@hotmail.com>
2023-02-05 14:11:53 +0100

    [Basic] Increase MaxPos.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- config/examples/Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 2.0/Configuration.h
+++ config/examples/Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 2.0/Configuration.h
@@ -1774,8 +1774,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 245
+#define Y_MAX_POS 240
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50

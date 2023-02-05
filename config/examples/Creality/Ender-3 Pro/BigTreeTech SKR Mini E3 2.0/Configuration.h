9cdc6e00555fe35b72beda51128b7d72403612de
Juri Berlanda <juriberlanda@hotmail.com>
2023-02-05 16:37:12 +0100

    [UI] Set CUSTOM_MACHINE_NAME.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- config/examples/Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 2.0/Configuration.h
+++ config/examples/Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 2.0/Configuration.h
@@ -137,7 +137,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 Pro"
+#define CUSTOM_MACHINE_NAME "Ender-3 Pro v2.1.2 (VoronM4)"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
